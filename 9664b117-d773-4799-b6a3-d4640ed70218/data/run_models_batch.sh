faadd3712dd0c778bb959b5f2984771793b16047
Pabashi Fernando <135970955+mphfernando@users.noreply.github.com>
2024-11-16 10:18:47 +0530

    Add files via upload

---
+++ 9664b117-d773-4799-b6a3-d4640ed70218/data/run_models_batch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
+#                                                                             
+# PROGRAMMER: Pabashi Fernando
+# DATE CREATED:                                  
+# REVISED DATE: - 
+# PURPOSE: Runs all three models to test which provides 'best' solution.
+#          Please note output from each run has been piped into a text file.
+#
+# Usage: sh run_models_batch.sh    -- will run program from commandline within Project Workspace
+#  
+#python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
+#python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
+#python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
+
+echo "Running ResNet model..."
+python check_images.py --dir pet_images/ --arch resnet --dogfile dognames.txt > resnet_pet-images.txt
+
+echo "Running AlexNet model..."
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
+
+echo "Running VGG model..."
+python check_images.py --dir pet_images/ --arch vgg --dogfile dognames.txt > vgg_pet-images.txt
